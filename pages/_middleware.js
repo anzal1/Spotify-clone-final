e91bb5d8881545dd49dd752f1a8344f7f6282038
anzal1 <you@example.com>
2022-03-18 16:20:50 +0530

    absolute url errror

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,15 +1,21 @@
 import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
+import absoluteUrl from "next-absolute-url";
 
 export async function middleware(req) {
+
+  const { origin } = absoluteUrl(req)
+
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  // const url = req.nextUrl.clone()
+  // url.pathname = '/dest'
   const { pathname } = req.nextUrl;
 
-  if (pathname.includes("/api/auth") || token) {
+  if (pathname?.includes("/api/auth") || token) {
     return NextResponse.next();
   }
 
   if (!token && pathname !== "/login") {
-    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/login`);
+    return NextResponse.redirect(origin+"/login");
   }
 }
